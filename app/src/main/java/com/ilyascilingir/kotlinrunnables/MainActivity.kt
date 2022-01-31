16bbc52d9d5bdf7f32f12de6d41860309ffe36bd
İlyas Çilingir <ilyas.cilingir@icisleri.gov.tr>
2022-01-31 15:56:20 +0300

    Initial commit

---
+++ app/src/main/java/com/ilyascilingir/kotlinrunnables/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.ilyascilingir.kotlinrunnables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.ilyascilingir.kotlinrunnables.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var number = 0
+    var runnable : Runnable = Runnable {  }
+    var handler : Handler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun start (view : View) {
+
+        number = 0
+        // initiliaze runnable
+        runnable = object : Runnable {
+            override fun run() {
+                number = number + 1
+                binding.textView.text = "Time : ${number}"
+
+                handler.postDelayed(this,1000)
+            }
+        }
+
+        handler.post(runnable)
+
+    }
+
+    fun stop (view : View) {
+
+        handler.removeCallbacks(runnable)
+        number = 0
+        binding.textView.text = "Time : 0"
+
+    }
+}
